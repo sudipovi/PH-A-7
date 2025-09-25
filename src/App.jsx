6fd5743919600d802677eb86824bb693ec93fece
sudipovi <shudipdey73@gmail.com>
2025-09-26 00:22:21 +0600

    nav and foot done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from "./assets/Components/Footer";
+import Navbar from "./assets/Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container mx-auto my-9">
+        <div></div>
+        <div></div>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
